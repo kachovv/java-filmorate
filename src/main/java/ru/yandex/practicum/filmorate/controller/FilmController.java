eb47587babf760e5facfa3fbf8f5b4d3572c98ce
Ekaterina Isakova <kachovv@yandex.ru>
2026-08-10 21:12:41 +0300

    Исправлены ошибки по результатам проверки на GitHub

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -3,17 +3,12 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
-import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
-import java.time.LocalDate;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RestController
--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorResponse.java
@@ -1,13 +1,12 @@
 package ru.yandex.practicum.filmorate.exception;
 
+import lombok.Getter;
+
+@Getter
 public class ErrorResponse {
     private final String error;
 
     public ErrorResponse(String error) {
         this.error = error;
-    }
-
-    public String getError() {
-        return error;
     }
 }
